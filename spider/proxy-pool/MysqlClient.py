a46cd1c874df03dfe1b45b24594e1560c19f82ae
yzy <492168579@qq.com>
2021-03-06 13:59:30 +0800

    修改编码

--- spider/proxy-pool/MysqlClient.py
+++ spider/proxy-pool/MysqlClient.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 MYSQL_HOST = 'localhost'
 MYSQL_PORT = 3306
 MYSQL_USER = 'root'
--- spider/proxy-pool/VerifyProxy.py
+++ spider/proxy-pool/VerifyProxy.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import requests
 from MysqlClient import MysqlClient
 
